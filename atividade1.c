78c3652bece2a0edb71baab42c33f7ea6640b0dd
Cristhiano Cunha <cristhiano.cunha@gmail.com>
2025-04-01 20:40:22 -0300

    Resolvendo problema de memoria

--- atividade1.c
+++ atividade1.c
@@ -1,46 +1,60 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 // Protótipos
 void cabecalho();
 float calcularMedia(float notas[], int quantidade);
 void imprimirResultado(float media);
 
-int main() {
+int main()
+{
     int quantidade = 2;
-    float notas[quantidade];
-    
     cabecalho();
     printf("Qual e quantidade de notas? \n");
     scanf("%d", &quantidade);
-   for (int i = 0; i < quantidade; ++i ){
+
+    float *notas = (float *)malloc(quantidade * sizeof(float));
+    if (notas == NULL)
+    {
+        printf("erro na alocação de memoria");
+        return 1;
+    }
+
+    for (int i = 0; i < quantidade; ++i)
+    {
         printf("Digite nota %d \n", i + 1);
         scanf("%f", &notas[i]);
     }
-    
-    // Chamando função que chama outra função
+
     imprimirResultado(calcularMedia(notas, quantidade));
-    
+    free(notas);
     return 0;
 }
 
-void cabecalho() {
+void cabecalho()
+{
     printf("=== CALCULADORA DE MÉDIAS ===\n\n");
 }
 
-float calcularMedia(float notas[], int quantidade) {
+float calcularMedia(float notas[], int quantidade)
+{
     float somasdasmedias = 0;
-    for (int i = 0; i < quantidade; ++i ){
+    for (int i = 0; i < quantidade; ++i)
+    {
         somasdasmedias += notas[i];
     }
-    
+
     return somasdasmedias / quantidade;
 }
 
-void imprimirResultado(float media) {
+void imprimirResultado(float media)
+{
     printf("\nMédia calculada: %.2f\n", media);
-    if(media >= 6) {
+    if (media >= 6)
+    {
         printf("Situação: Aprovado!\n");
-    } else {
+    }
+    else
+    {
         printf("Situação: Reprovado\n");
     }
 }
