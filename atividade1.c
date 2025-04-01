0de4be5b57114954918114479d06f9547bfae6e8
Cristhiano Cunha <cristhiano.cunha@gmail.com>
2025-04-01 20:29:49 -0300

    criando com malloc

--- atividade1.c
+++ atividade1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h> // Para malloc()
 
 // Protótipos
 void cabecalho();
@@ -6,20 +7,33 @@ float calcularMedia(float notas[], int quantidade);
 void imprimirResultado(float media);
 
 int main() {
-    int quantidade = 2;
-    float notas[quantidade];
-    
+    int quant;
+
     cabecalho();
-    printf("Qual e quantidade de notas? \n");
-    scanf("%d", &quantidade);
-   for (int i = 0; i < quantidade; ++i ){
-        printf("Digite nota %d \n", i + 1);
+    printf("Qual é a quantidade de notas? \n");
+    scanf("%d", &quant);
+
+    if (quant <= 0) {
+        printf("Erro: A quantidade de notas deve ser maior que zero.\n");
+        return 1; // Sai do programa com erro
+    }
+
+    // Alocação dinâmica para armazenar as notas
+    float *notas = (float *)malloc(quant * sizeof(float));
+    if (notas == NULL) {
+        printf("Erro de alocação de memória!\n");
+        return 1; // Sai do programa com erro
+    }
+
+    for (int i = 0; i < quant; ++i) {
+        printf("Digite a nota %d: ", i + 1);
         scanf("%f", &notas[i]);
     }
-    
+
     // Chamando função que chama outra função
-    imprimirResultado(calcularMedia(notas, quantidade));
-    
+    imprimirResultado(calcularMedia(notas, quant));
+
+    free(notas); // Liberando memória alocada
     return 0;
 }
 
@@ -28,8 +42,8 @@ void cabecalho() {
 }
 
 float calcularMedia(float notas[], int quantidade) {
-    float somasdasmedias = 0;
-    for (int i = 0; i < quantidade; ++i ){
+    float somasdasmedias = 0; // Inicialização correta
+    for (int i = 0; i < quantidade; ++i) {
         somasdasmedias += notas[i];
     }
     
@@ -38,7 +52,7 @@ float calcularMedia(float notas[], int quantidade) {
 
 void imprimirResultado(float media) {
     printf("\nMédia calculada: %.2f\n", media);
-    if(media >= 6) {
+    if (media >= 7) {
         printf("Situação: Aprovado!\n");
     } else {
         printf("Situação: Reprovado\n");
